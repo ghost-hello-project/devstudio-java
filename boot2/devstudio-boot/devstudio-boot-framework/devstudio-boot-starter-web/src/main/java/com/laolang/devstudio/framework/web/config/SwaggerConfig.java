6d6b99e30c226b1d9198370147129d28f8e61585
laolang2016 <xiaodaima2016@163.com>
2026-06-21 22:26:44 +0800

    boot2(boot): 格式化代码

--- boot2/devstudio-boot/devstudio-boot-framework/devstudio-boot-starter-web/src/main/java/com/laolang/devstudio/framework/web/config/SwaggerConfig.java
+++ boot2/devstudio-boot/devstudio-boot-framework/devstudio-boot-starter-web/src/main/java/com/laolang/devstudio/framework/web/config/SwaggerConfig.java
@@ -9,24 +9,21 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
     @Bean
     public GroupedOpenApi systemApi() {
-        String[] paths = {"/**"};
-        String[] packagedToMatch = {"com.laolang.devstudio.module.system.controller"};
-        return GroupedOpenApi.builder().group("system")
-                .pathsToMatch(paths)
-                .packagesToScan(packagedToMatch).build();
+        String[] paths = { "/**" };
+        String[] packagedToMatch = { "com.laolang.devstudio.module.system.controller" };
+        return GroupedOpenApi.builder().group("system").pathsToMatch(paths).packagesToScan(packagedToMatch).build();
     }
 
     @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("thresh")
-                        .version("0.1")
-                        .description("本地开发工作室")
-                        .termsOfService("https://github.com/laolang2016")
-                        .license(new License().name("MIT").url("https://github.com/laolang2016"))
-                );
+        return new OpenAPI().info(new Info().title("thresh")
+            .version("0.1")
+            .description("本地开发工作室")
+            .termsOfService("https://github.com/laolang2016")
+            .license(new License().name("MIT").url("https://github.com/laolang2016")));
     }
+
 }
